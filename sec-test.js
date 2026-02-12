103207f97c2124c1eddf3bb4d07f71223bc5cf59
jayanth.jagadish <jayanth.jagadish@relevantz.com>
2026-02-12 16:22:50 +0530

    fixed

--- sec-test.js
+++ sec-test.js
@@ -2,7 +2,7 @@
 const API_SECRET = "my-super-secret-key-12345";
 const pwd = "admin123";
 function login(password) {
-    if (password === pwd) {
+    if (password == pwd) {
         return "Access granted";
     }
     return "Access denied";
